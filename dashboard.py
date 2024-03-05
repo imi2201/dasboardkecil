ff77099ca1df11ed484d35170929aa87f3cf2527
imi2201 <142393272+imi2201@users.noreply.github.com>
2024-03-05 16:08:57 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -13,7 +13,7 @@ product_df = pd.read_csv('https://raw.githubusercontent.com/imi2201/dasboardkeci
 st.title('Dashboard E-Commerce')
 
 # Create two columns
-col1, col2 = st.columns(2)
+col1, col2 = st.column(2)
 
 # Display seller data in the first column
 with col1:
@@ -24,3 +24,18 @@ with col1:
 with col2:
     st.write("Product Data")
     st.write(product_df)
+
+st.write("diberikan data tersebut akan di cari:")
+col3,col4 = st column(2)
+
+with col3:
+    st.write("pertanyaan 1 akan ditunjukkan 10 product")
+    st.write("yang memiliki tingkat pembelian tertinggi")
+    category_counts = product_df.groupby("product_category_name")["order_id"].count()
+    top_categories = category_counts.sort_values(ascending=False).head(10)
+    plt.barh(y=top_categories.index, width=top_categories.values)
+    plt.xlabel('Count')  # Label for x-axis
+    plt.ylabel('Product Category')  # Label for y-axis
+    plt.title('Top 10 Product Ordered')
+    st.pyplot(plt)
+    plt.clf()
